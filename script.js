87c0ef1d4a58ac1a6f4fd0122833016b0d567999
Jainish Devpura <jainishdevpura24@gmail.com>
2025-05-13 23:23:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Menu Toggle for Mobile
+document.getElementById('mobile-nav-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
